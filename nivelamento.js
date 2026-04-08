26ac12fd0798045922ee2dd443c748de8c94e4f0
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-04-08 11:52:22 -0300

    Add files via upload

---
+++ nivelamento.js
@@ -0,0 +1,24 @@
+// nome: Joaquim Custódio Neto
+// 1. exiba a mensagem "Olá Mundo" no console
+4
+console.log("Olá Mundo");
+// 2. crie uma variável com seu nome e exiba o valor no no console
+const nome = 'Joaquim';
+console.log(nome);
+// 3. Some dois números e exiba o resultado
+let num1 = 5;
+let num2 = 10;
+console.log(num1 + num2);
+// 4. Subtraia dois números e exiba o valor.
+let sub = 10-20;
+console.log(sub);
+// 5. descubra o resto da divisão de um número por outro.
+let resto = 15%3;
+console.log(resto);
+// 6. crie uma variável booleana(true false) e exiba seu valor.
+let bool = true;
+console.log(bool);
+// 7. Verifique se um número é maior que outro e exiba o resultado.
+if(10>3){
+    console.log('É maior')
+}
