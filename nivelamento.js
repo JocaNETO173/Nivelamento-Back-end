219f6439e006f8ccc11d95ecb682bf6eaf0085c3
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-04-09 09:09:02 -0300

    Add files via upload

--- nivelamento.js
+++ nivelamento.js
@@ -1,6 +1,5 @@
 // nome: Joaquim Custódio Neto
 // 1. exiba a mensagem "Olá Mundo" no console
-4
 console.log("Olá Mundo");
 // 2. crie uma variável com seu nome e exiba o valor no no console
 const nome = 'Joaquim';
@@ -22,4 +21,43 @@ console.log(bool);
 if(10>3){
     console.log('É maior')
 }
-// 8.
+// 8. Crie duas variáveis com algum valor e exiba a concatenação
+let var1 = 'Varíavel 1';
+let var2 = 'Variável 2';
+console.log(`mostrando a ${var1} e ${var2}`); // <--- concatenação é mostrar múltiplas variáveis de uma só vez.
+// 9. descubra o tipo de uma variável
+let num3 = 5;
+console.log(typeof num3)
+// 10. Converta número para string.
+let numero = String(50);
+console.log(typeof numero)
+// 11. string pra numero
+let texto = '123';
+Number(texto)
+
+// laços de repetição
+
+// 12. exiba no console número de 0 a 10
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+// 13. Exiba apenas números pares até 20
+for(let i=2;i<=20;i+=2){
+    console.log(i);
+}
+// 14. Mostre no console numero de 10 até 1.
+for(let i=10;i>=1;i--){
+    console.log(i)
+}
+// 15. Some números de 1 a 5.
+let x = 0
+for(let i=1;i<=5;i++){
+    x+=i;
+    console.log(x)
+}
+// 16. faça a tabuada do 3.
+for(let i=1;i<=10;i++){
+    x=3;
+    const multi = i * x;
+    console.log(`${i} x 3 = ${multi}`);
+}
