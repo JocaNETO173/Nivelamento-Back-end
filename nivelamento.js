ce0486770c449978688f73c08225d3f9f8ad8041
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-04-15 11:51:39 -0300

    Add files via upload

--- nivelamento.js
+++ nivelamento.js
@@ -135,31 +135,112 @@ console.log(15 > 0);
 console.log(15>0? "par" : "impar");
 //com if e else
 if(15>0){
-    console.log('15 é positivo')
-}
-else{
-    console.log('15 é negativo')
-}
+    console.log('15 é positivo');
+} else {
+    console.log('15 é negativo');
+};
 
 //26. verifique se uma pessoa com 17 anos é maior de idade
-let idade = 17
+let idade = 17;
 if(idade >= 18){
-    console.log(`Esta pessoa tem ${idade} anos e é maior de idade`)
+    console.log(`Esta pessoa tem ${idade} anos e é maior de idade`);
 } else {
-    console.log(`Essa pessoa tem ${idade} anos, ou seja, é menor de idade`)
+    console.log(`Essa pessoa tem ${idade} anos, ou seja, é menor de idade`);
 };
 // 27. Crie um array com os números 10, 20, 30, 40 e exiba todos.
-let array_numeros = [10, 20, 30, 40]
-console.log(array_numeros)
+let arrayNum = [10, 20, 30, 40];
+console.log(arrayNum);
 
 // 28. Crie um array com 5 frutas e exiba a primeira fruta.
 
-let frutas = ['abacate', 'banana', 'caju', 'damasco', 'pitaia']
-console.log(frutas[0])
+let frutas = ['abacate', 'banana', 'caju', 'damasco', 'pitaia'];
+console.log(` a primeira fruta do array é ${frutas[0]}`);
 
 //29. adicione o número 50 no final do array [10, 20, 30, 40]
-let array_numeros1 = [10,20,30,40]
-array_numeros1.push(50)
-console.log(array_numeros1)
+let arrayNum1 = [10,20,30,40];
+arrayNum1.push(50);
+console.log(arrayNum1);
 
-//30. Remova 
+//30. Remova o último elemento do array [1, 2, 3, 4, 5]
+let arrayNum2 = [1, 2, 3, 4, 5];
+arrayNum2.pop();
+console.log(arrayNum2);
+
+//31. Exiba quantos elementos tem o array [5, 10, 15, 20]
+let arrayNum3 = [5,10,15,20];
+console.log(`Este array tem ${arrayNum3.length} elementos`);
+
+// 32: Some todos os números do array [2, 4, 6, 8]
+let arrayNum4 = [2,4,6,8]
+soma = 0;
+for(let i =0;i<arrayNum4.length;i++){
+    soma += arrayNum4[i]
+}
+console.log(`A soma de todos os números do array [${arrayNum4}] é ${soma}`)
+// 33: Multiplique cada elemento do array [1, 2, 3, 4] por 2
+let arrayNum5=[1,2,3,4];
+let arrayNum5Mult=[];
+for(let i=0;i<arrayNum5.length;i++){
+   arrayNum5Mult.push(arrayNum5[i] * 2)
+}
+console.log(`o array [${arrayNum5}] multiplicado por 2, fica [${arrayNum5Mult}]`)
+
+// 34: Verifique se a fruta "laranja" existe no array ["maçã", "banana", "uva"]
+let frutas2 = ["maçã","banana","uva"]
+for(let i=0;i<frutas2.length;i++){
+    if(frutas2[i] === "laranja"){
+        console.log("Laranja existe nesse array")
+    } else {
+        console.log("Laranja não existe nesse array")
+    }
+}
+
+// 35: Exiba um por um os elementos do array ["azul", "verde", "amarelo"]
+
+// 36: Exiba o último elemento do array [100, 200, 300, 400, 500]
+
+
+// ==================== ATIVIDADES COM MATRIZ ====================
+
+// 37: Crie uma matriz 2x2 com os números 1, 2 na primeira linha e 3, 4 na segunda
+
+// 38: Exiba o elemento da linha 1, coluna 2 da matriz [[5, 6], [7, 8]]
+
+// 39: Exiba todos os elementos da matriz [[1, 2, 3], [4, 5, 6]]
+
+// 40: Some todos os números da matriz [[2, 4], [6, 8]]
+
+// 41: Crie uma matriz 3x3 com números de 1 a 9
+
+// 42: Exiba os elementos da diagonal principal da matriz [[1,2,3],[4,5,6],[7,8,9]]
+
+// 43: Altere o elemento da linha 0, coluna 1 da matriz [[1,2],[3,4]] para 10
+
+// 44: Exiba apenas a primeira linha da matriz [[10,20],[30,40],[50,60]]
+
+// 45: Exiba apenas os elementos da primeira coluna da matriz [[1,2],[3,4],[5,6]]
+
+// 46: Multiplique todos os elementos da matriz [[1,2],[3,4]] por 2
+
+
+// ==================== ATIVIDADES COM OBJETOS ====================
+
+// 47: Crie um objeto pessoa com nome "Ana" e idade 25
+
+// 48: Acesse e exiba o nome do objeto {nome: "Carlos", cidade: "São Paulo"}
+
+// 49: Adicione a propriedade "curso" ao objeto {nome: "Maria", idade: 20}
+
+// 50: Altere a idade do objeto {nome: "João", idade: 18} para 19
+
+// 51: Remova a propriedade "telefone" do objeto {nome: "Pedro", telefone: "99999-9999"}
+
+// 52: Exiba todas as propriedades do objeto {marca: "Nike", tamanho: 42, cor: "preto"}
+
+// 53: Crie um objeto cachorro com nome "Rex" e um método latir que exibe "Au au!"
+
+// 54: Crie um objeto aluno com nome "Lucas" e notas [8, 7, 9]
+
+// 55: Crie um objeto carro com marca "Fiat" e um objeto motor com potencia "1.0"
+
+// 56: Crie um array com 3 objetos de produtos (nome e preço) e exiba todos
